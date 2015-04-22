2d6728163504f7b1e40a1d96b202a46b9e0912ec
Courtney Phillips & Kim Dang <student@epicodus.com>
2015-04-22 14:54:37 -0700

    Add false test.

--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -1,7 +1,7 @@
 require('rspec')
 require('./lib/rock_paper_scissors')
 
-describe('String#beats') do
+describe('String#beats?') do
   it("returns true if rock is the object and scissors is the argument") do
     expect("rock".beats?("scissors")).to(eq(true))
   end
@@ -10,4 +10,7 @@ describe('String#beats') do
     expect("scissors".beats?("paper")).to(eq(true))
   end
 
+  it("returns false if rock is the object and paper is the argument") do
+    expect("rock".beats?("paper")).to(eq(false))
+  end
 end
