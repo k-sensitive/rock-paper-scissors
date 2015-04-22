3f697d790ecfc051664a37f1603f53347f9f9002
Courtney Phillips & Kim Dang <student@epicodus.com>
2015-04-22 14:51:50 -0700

    Modified spec.

--- spec/rock_paper_scissors_integration_spec.rb
+++ spec/rock_paper_scissors_integration_spec.rb
@@ -1,1 +1,13 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
 
+describe('the rock paper scissor path', {:type => :feature}) do
+  it('processes the user entries and returns a winner') do
+    visit('/')
+    fill_in('player1', :with => 'rock')
+    fill_in('player2', :with => 'scissors')
+    click_button('Fight!')
+    expect(page).to have_content(true)
+  end
+end
